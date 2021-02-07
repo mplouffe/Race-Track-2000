92c0ee3ad3bfc7fcf3642567148dcef6b6405d9f
mplouffe <matheuplouffe@gmail.com>
2021-02-06 21:47:20 -0600

    Added HomeOptions component and associated media.

---
+++ public/racetrack-2000/src/components/HomeOptions.vue
@@ -0,0 +1,103 @@
+<template>
+    <v-container grid-list-lg>
+        <v-layout row>
+            <v-flex xs12 class="text-xs-center display-1 font-weight-black my-5"
+                >So Many Ways To Win</v-flex
+            >
+        </v-layout>
+        <v-layout row wrap>
+            <v-flex xs12 sm12 md4>
+                <v-card>
+                    <v-img src="@/assets/betRaceCard.jpg" height="500px">
+                        <v-container fill-height fluid>
+                            <v-layout fill-height>
+                                <v-flex xs12 aligh-end flexbox>
+                                    <span class="headline white--text"
+                                        >BET</span
+                                    >
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </v-img>
+
+                    <v-card-title primary-title>
+                        <div>
+                            <h3 class="headline mb-0">Bet</h3>
+                            <div>
+                                Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit. Nullam mauris felis, varius
+                                rutrum massa a, dignissim ornare dui. Cras eget
+                                velit eu dui tristique lobortis sit amet vel
+                                tellus.
+                            </div>
+                        </div>
+                    </v-card-title>
+                </v-card>
+            </v-flex>
+
+            <v-flex xs12 sm12 md4>
+                <v-card>
+                    <v-img src="@/assets/ownHorseCard.jpg" height="500px">
+                        <v-container fill-height fluid>
+                            <v-layout fill-height>
+                                <v-flex xs12 align-end flexbox>
+                                    <span class="headline white--text"
+                                        >OWN</span
+                                    >
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </v-img>
+
+                    <v-card-title primary-title>
+                        <div>
+                            <h3 class="headline mb-0">Own</h3>
+                            <div>
+                                Take your winnings from the track and invest in
+                                your own horse! When your horse wins, you win.
+                                Sell breeding rights and have your winner sire
+                                the next generation of champions!
+                            </div>
+                        </div>
+                    </v-card-title>
+                </v-card>
+            </v-flex>
+
+            <v-flex xs12 sm12 md4>
+                <v-card>
+                    <v-img src="@/assets/ponyFightClubCard.jpg" height="500px">
+                        <v-container fill-height fluid>
+                            <v-layout fill-height>
+                                <v-flex xs12 align-end flexbox>
+                                    <span class="headline white--text"
+                                        >PONY FIGHTS</span
+                                    >
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </v-img>
+
+                    <v-card-title primary-title>
+                        <div>
+                            <h3 class="headline mb-0">Pony Fight Club</h3>
+                            <div>
+                                Start your own underground pony fight club! Just
+                                remember that the first rule of pony fight club
+                                is that you do not talk about pony fight club.
+                                The second rule is "Neigh"!
+                            </div>
+                        </div>
+                    </v-card-title>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: "HomeOptions"
+};
+</script>
+
+<style scoped></style>
--- public/racetrack-2000/src/views/Home.vue
+++ public/racetrack-2000/src/views/Home.vue
@@ -2,18 +2,21 @@
     <span>
         <home-hero></home-hero>
         <home-details></home-details>
+        <home-options></home-options>
     </span>
 </template>
 
 <script>
 import HomeHero from "@/components/HomeHero";
 import HomeDetails from "@/components/HomeDetails";
+import HomeOptions from "@/components/HomeOptions";
 
 export default {
     name: "Home",
     components: {
         HomeHero,
-        HomeDetails
+        HomeDetails,
+        HomeOptions
     }
 };
 </script>
