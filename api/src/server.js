b3e013d76ade942da30894ab66e6f68a5134afc6
Matheu Plouffe <matheuplouffe@gmail.com>
2021-02-13 12:14:20 -0600

    Set up server to serve Vue app.

--- api/src/server.js
+++ api/src/server.js
@@ -1,15 +1,35 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 
-const publicPath = path.join(__dirname, '../public');
+const users = [];
+
+const publicPath = path.join(__dirname, '../../racetrack-2000/dist');
 const port = process.env.PORT || 3000;
 
 const app = express();
-const server = http.createServer(app);
 
 app.use(express.static(publicPath));
+app.use(bodyParser.json());
 
+app.get('/api/users', (req, res) => {
+    console.log('api/users called!');
+    res.json(users);
+});
+
+app.post('/api/user', (req, res) => {
+    const user = req.body.user;
+    console.log('Adding user:::', user);
+    users.push(user);
+    res.json("user added");
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../racetrack-2000/dist/index.html'));
+});
+
+const server = http.createServer(app);
 server.listen(port, (err) => {
     console.log(`RaceTrack 2000 is up on localhost:${port}`);
 });
